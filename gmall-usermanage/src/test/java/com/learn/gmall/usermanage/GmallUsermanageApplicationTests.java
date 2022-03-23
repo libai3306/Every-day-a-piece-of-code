140a01577bd013e4c135cdef668dcac8e68c2fbe
高峰 <1398315194@qq.com>
2022-03-23 11:02:18 +0800

    one day 搭建未完成

---
+++ gmall-usermanage/src/test/java/com/learn/gmall/usermanage/GmallUsermanageApplicationTests.java
@@ -0,0 +1,17 @@
+package com.learn.gmall.usermanage;
+
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Ignore
+@SpringBootTest
+class GmallUsermanageApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
